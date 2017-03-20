9aa8ca90fc201d5ef35018e5172f98e2fb837a02
lilitom <ahguohao11@163.com>
2017-03-20 19:06:23 +0800

    again

--- optimization method.py
+++ optimization method.py
@@ -22,6 +22,29 @@ def readfile(fileneme):
     labelMat=np.reshape(np.array(labelMat),[np.shape(dataMat)[0],1])
     return dataMat,labelMat
 
+#the plot function which used to plot the figure plot the dataset and the splitline using matplotlib
+def plotBestFit(weights,filename='testSet.txt'):
+    import matplotlib.pyplot as plt
+    dataMat,labelMat=readfile(filename)
+    n = np.shape(dataMat)[0]
+    xcord1 = []; ycord1 = []
+    xcord2 = []; ycord2 = []
+    for i in range(n):
+        if int(labelMat[i])== 1:
+            xcord1.append(dataMat[i,1]); ycord1.append(dataMat[i,2])
+        else:
+            xcord2.append(dataMat[i,1]); ycord2.append(dataMat[i,2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = np.arange(-3.0, 3.0, 0.1)
+    y = (-weights[0]-weights[1]*x)/weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1'); plt.ylabel('X2');
+    plt.show()
+    
+    
 #A sigmoid function is a mathematical function having an "S" shaped curve
 def sigmoid(input_x):
     return 1.0/(1+np.exp(-input_x))
@@ -65,27 +88,6 @@ def stocGradAscent1(input_mat,label_mat,alpha=0.001,numIter=500):
             del (dataIndex[randIndex])
     return weight
 
-#the plot function which used to plot the figure plot the dataset and the splitline using matplotlib
-def plotBestFit(weights,filename='testSet.txt'):
-    import matplotlib.pyplot as plt
-    dataMat,labelMat=readfile(filename)
-    n = np.shape(dataMat)[0]
-    xcord1 = []; ycord1 = []
-    xcord2 = []; ycord2 = []
-    for i in range(n):
-        if int(labelMat[i])== 1:
-            xcord1.append(dataMat[i,1]); ycord1.append(dataMat[i,2])
-        else:
-            xcord2.append(dataMat[i,1]); ycord2.append(dataMat[i,2])
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
-    ax.scatter(xcord2, ycord2, s=30, c='green')
-    x = np.arange(-3.0, 3.0, 0.1)
-    y = (-weights[0]-weights[1]*x)/weights[2]
-    ax.plot(x, y)
-    plt.xlabel('X1'); plt.ylabel('X2');
-    plt.show()
 
 #the main function to debug
 if __name__=="__main__":
